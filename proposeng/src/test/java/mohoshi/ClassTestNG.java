1b5917494d9de20c2a1e2a6892e39c69f3888418
TCC <TCC@DESKTOP-CO84QLD>
2023-09-30 21:52:37 +0600

    First Commit

---
+++ proposeng/src/test/java/mohoshi/ClassTestNG.java
@@ -0,0 +1,14 @@
+package mohoshi;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ClassTestNG {
+	
+
+}
